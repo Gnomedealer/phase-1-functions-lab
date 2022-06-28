11b7fe8de4c36a6c57b976d281e590feedc61a18
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-28 09:56:41 -0700

    commit tree dff137a5790a89cdd1468b1a4f208a77782c3974

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(42 - someValue)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264 
+}
+
+function calculatesFarePrice(start, destination) {
+    let x = distanceTravelledInFeet(start, destination)
+    if (x <= 400){
+        return 0
+    }
+    if (x > 400 && x <= 2000){
+        return 0.02 * (x - 400)
+    }
+    if (x > 2000 && x < 2500){
+        return 25
+    }
+    if (x > 2500)
+        return 'cannot travel that far'
+  }
